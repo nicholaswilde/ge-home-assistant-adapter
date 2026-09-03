3f7348480f1b51950a67c48edee8fed9225780a1
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2026-09-03 06:47:39 -0700

    feat(mqtt): add home assistant discovery config

--- src/mqtt_manager.cpp
+++ src/mqtt_manager.cpp
@@ -38,6 +38,11 @@ void connectToMqtt(HomeAssistantBridge& bridge)
     if(mqttClient.connect(deviceId, mqttUser, mqttPassword)) {
       Serial.println("connected");
       digitalWrite(LED_MQTT, HIGH);
+
+      String discoveryTopic = String("homeassistant/sensor/") + String(deviceId) + String("/config");
+      String payload = String("{\"name\": \"") + String(deviceId) + String("\", \"state_topic\": \"") + String(deviceId) + String("/status\", \"unique_id\": \"") + String(deviceId) + String("\", \"device\": {\"identifiers\": [\"") + String(deviceId) + String("\"], \"name\": \"") + String(deviceId) + String("\"}}");
+      mqttClient.publish(discoveryTopic.c_str(), payload.c_str(), true);
+      mqttClient.publish((String(deviceId) + "/status").c_str(), "online", true);
     }
     else {
       Serial.println("failed, rc=" + String(mqttClient.state()) + " will try again in 1 second");
